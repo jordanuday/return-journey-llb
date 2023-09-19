06272d3a29561689d5491d1b276979c6243dffaf
GURRAM UDAY KUMAR REDDY <73101081+jordanuday@users.noreply.github.com>
2023-09-19 06:41:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import GreenLightRedLight from "./components/GreenLightRedLight";
+
+const App = () => {
+  return (
+    <div>
+      <GreenLightRedLight />
+    </div>
+  );
+};
+
+export default App;
